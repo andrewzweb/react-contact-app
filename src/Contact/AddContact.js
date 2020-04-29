faaa1b6a7f732cafe3ccc3b227bdb1b233e7efef
andrewzweb <andrewzweb@gmail.com>
2020-04-29 22:59:50 +0300

    Make form AddContact more pretty

--- src/Contact/AddContact.js
+++ src/Contact/AddContact.js
@@ -18,15 +18,14 @@ function AddContact({onCreate}){
 	}
   }
 
-
 	return(
 		<div>
 		<div className='add-contact-button'>
 		  <Icon>person_add</Icon>
 		</div>
-		<form onSubmit={submitHandler}>
-		  <input value={name} onChange={event => setName(event.target.value)}/>
-		  <input value={phone} onChange={event => setPhone(event.target.value)}/>
+		<form className='form-add-contact' onSubmit={submitHandler}>
+		  <input value={name} onChange={event => setName(event.target.value)} placeholder="Name"/>
+		  <input value={phone} onChange={event => setPhone(event.target.value)}  placeholder="Number"/>
 		  <button type='submit'>Create Contact</button>
 		</form>
 		</div>
